e280de67ca41b670b230989cad90e571179a8c0e
Veera Pirla <17177346+veera83372@users.noreply.github.com>
2021-03-08 22:06:49 +0530

    Add script for generating book

---
+++ gb.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -d src ]; then
+    mkdir src
+fi
+
+printf '[Introduction](introduction.md)\n\n' > src/SUMMARY.md
+
+find ./text ! -type d -print0 | xargs -0 -I {} cp {} src/
+
+
+find ./src ! -type d -name '*.md' -print0 \
+  | sort -z \
+  | while read -r -d '' file;
+do
+    printf -- '- [%s](%s)\n' "$(basename "$file" ".md")" "$(basename "$file")" 
+done >> src/SUMMARY.md
+
+
+
+cp README.md src/introduction.md
+
+mdbook build
+mkdir -p book/img/
+find ./book ! -type d -name '*.png' -print0 | xargs -0 -I {} cp {} book/img/
